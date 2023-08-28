b5f80dbb445456debe39617f4b4ed62e81d4a29e
neeswebservice <neeswebservice@gmail.com>
2023-08-28 12:38:06 +0545

    removed-default-locale

--- extension.js
+++ extension.js
@@ -1,49 +1,47 @@
-const vscode = require('vscode');
-const moment = require('moment');
-moment.locale('id');
-  
+const vscode = require("vscode");
+const moment = require("moment");
+
 var interval, item;
 var show = false;
 
 function toggleImplementation() {
-	if (show) {
-		if (item) {
-			item.hide();
-		}
-		if (interval) {
-			clearInterval(interval);
-			interval = null;
-		}
-	} else {
-		if (!item) item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -100);
-		item.show();
-		interval = setInterval(() => item.text = moment().format('Do MMMM YYYY, HH:mm:ss'), 1000);
-	}
-	show = !show;
+  if (show) {
+    if (item) {
+      item.hide();
+    }
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+  } else {
+    if (!item) item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -100);
+    item.show();
+    interval = setInterval(() => (item.text = moment().format("Do MMMM YYYY, HH:mm:ss")), 1000);
+  }
+  show = !show;
 }
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
+  console.log("status-bar-time extensions activated...");
 
-	console.log('status-bar-time extensions activated...');
+  let toggle = vscode.commands.registerCommand("status-bar-time.toggle", toggleImplementation);
 
-	let toggle = vscode.commands.registerCommand('status-bar-time.toggle', toggleImplementation);
+  context.subscriptions.push(toggle);
 
-	context.subscriptions.push(toggle);
-
-	toggleImplementation();
+  toggleImplementation();
 }
 exports.activate = activate;
 
 function deactivate() {
-	if (interval) {
-		clearInterval(interval);
-	}
+  if (interval) {
+    clearInterval(interval);
+  }
 }
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
